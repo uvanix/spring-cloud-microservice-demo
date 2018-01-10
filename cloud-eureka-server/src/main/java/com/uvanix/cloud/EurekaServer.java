449c30abf97cd140dfb132ae3105e406e79245f7
uvanix <uvans5233@gmail.com>
2018-01-10 18:29:42 +0800

    add eureka server and test

---
+++ cloud-eureka-server/src/main/java/com/uvanix/cloud/EurekaServer.java
@@ -0,0 +1,19 @@
+package com.uvanix.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author uvanix
+ * @title 程序入口-服务注册
+ * @date 2017/12/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
