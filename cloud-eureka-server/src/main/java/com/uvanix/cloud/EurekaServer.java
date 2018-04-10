aab9f8d165478855763acd9f8411a1c2e60415df
Yvan <chenyvan20180328@gmail.com>
2018-04-10 23:31:21 +0800

    fix eureka server cluster

--- cloud-eureka-server/src/main/java/com/uvanix/cloud/EurekaServer.java
+++ cloud-eureka-server/src/main/java/com/uvanix/cloud/EurekaServer.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author uvanix
- * @title 程序入口-服务注册
+ * @title 程序入口 - 服务注册中心
  * @date 2017/12/26
  */
 @SpringBootApplication
