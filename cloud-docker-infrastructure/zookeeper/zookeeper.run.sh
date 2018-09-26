ebd931f125c779c0231110404a6d906e1eb0a92f
uvanix <uvans5233@gmail.com>
2018-09-26 20:47:54 +0800

    add pinpoint docker env test

--- cloud-docker-infrastructure/zookeeper/zookeeper.run.sh
+++ cloud-docker-infrastructure/zookeeper/zookeeper.run.sh
@@ -6,7 +6,6 @@ mkdir -p conf logs data
 echo "docker run --name zookeeper ... -d zookeeper"
 docker run \
   --name zookeeper \
-  --restart always \
   -p 2181:2181 \
   -v $PWD/data:/data \
   -v $PWD/conf:/conf \
